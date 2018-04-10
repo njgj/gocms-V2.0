d2d68f14c4dfbfa0d092ca97c797ea0de5286379
gongjun <gongjun123321@163.com>
2018-04-10 18:33:46 +0800

    前台fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,5 +1,6 @@
 <?php
 namespace app\index\controller;
+use think\Validate;
 
 class Index extends \think\Controller
 {
@@ -7,4 +8,25 @@ class Index extends \think\Controller
     {
         return $this->fetch('index');
     }
+	
+    public function guestbook()
+    {
+       $data = input('post.');
+        $rule = [
+            'realname|姓名' => 'require|min:2',
+            'tel|电话' => 'require',
+            'email|邮箱' => 'require|email',
+            'content|留言' => 'require|max:300',
+        ];
+        //$validate = Validate::make($rule, $msg);
+        $validate = new Validate($rule);
+        if (!$validate->check($data)) {
+            return $validate->getError();
+        } else {
+            $data['title']=input('post.realname');
+            $data['ip']=request()->ip();
+            $data['addtime']=date('Y-m-d H:i:s');
+            return model('Zixun')->save($data);            
+		}
+    }	
 }
